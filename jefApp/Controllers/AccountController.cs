973cfee0c1861acebe3c5091ec8440df07191b4d
jefenso <jaespiritu@student.apc.edu.ph>
2023-09-13 07:04:48 +0800

    WebApp

---
+++ jefApp/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using jefApp.Models.EntityManager;
+using jefApp.Models.ViewModel;
+
+namespace jefApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                if (!um.IsLoginNameExist(user.LoginName))
+                {
+                    um.AddUserAccount(user);
+                    // FormsAuthentication.SetAuthCookie(user.FirstName, false);
+                    return RedirectToAction("", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            var users = new UserManager().GetAllUsers();
+
+            return View();
+        }
+    }
+}
